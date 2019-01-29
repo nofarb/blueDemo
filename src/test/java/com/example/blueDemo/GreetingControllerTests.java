9a357bf557f49b83a2743c9c0b4a1cf9eccbd894
blue <nofarb@gmail.com>
2019-01-29 09:13:30 -0500

    working app

--- src/test/java/com/example/blueDemo/GreetingControllerTests.java
+++ src/test/java/com/example/blueDemo/GreetingControllerTests.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -16,18 +15,24 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class BlueDemoApplicationTests {
+public class GreetingControllerTests {
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
 
+        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, World!"));
+    }
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void paramGreetingShouldReturnTailoredMessage() throws Exception {
 
-
+        this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
+    }
 
 }
-
